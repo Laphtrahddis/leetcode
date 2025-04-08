713023a291bfdeff8e8d56fb29f19a77eecdb96e
Siddharth Pal <130787774+Laphtrahddis@users.noreply.github.com>
2025-04-08 20:03:59 +0530

    Time: 3 ms (14.84%) | Memory: 12.7 MB (64.49%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        split=-1
+        n=len(nums)
+        if len(nums)==1:
+            if nums[0]==target:
+                return True
+            else: 
+                return False
+        for i in range(len(nums)-1):
+            if nums[i+1]<nums[i]:
+                split=i
+        if split!=-1:
+            nums=nums[split+1:n]+nums[0:split+1]
+        #print(nums)
+        found=-1
+        low=0
+        high=n-1
+        while(low<=high):
+            mid=(low+high)//2
+            if nums[mid]==target:
+                found=mid
+                break
+            elif nums[mid]<target:
+                low=mid+1
+            else:
+                high=mid-1
+        if found==-1:
+            return False
+        elif split==-1:
+            print("debug")
+            if found==-1:
+                return False
+            else:
+                return True
+        else:
+            return True
+        
+
+        

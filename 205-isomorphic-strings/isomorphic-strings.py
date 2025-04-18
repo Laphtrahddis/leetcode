15a7352720f28392fc9f1fd5430fa95e83c08249
Siddharth Pal <130787774+Laphtrahddis@users.noreply.github.com>
2025-04-18 16:52:52 +0530

    Time: 5 ms (94.31%) | Memory: 14 MB (36.14%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s=list(s)
+        t=list(t)
+        if len(s)!=len(t): return False
+        prevs=s[0]
+        prevt=t[0]
+        dict={}
+        for i in range(len(s)):
+            if s[i] not in dict:
+                if t[i] not in dict.values():
+                    dict[s[i]]=t[i]
+                else:
+                    return False
+            else:
+                if dict[s[i]]!=t[i]:
+                    return False
+        return True
+        
